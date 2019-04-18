12a6b73c836523ae9c24bdc845ecf06be36ff4d0
farisdx66 <farisdx66@gmail.com>
2019-04-18 07:38:53 +0700

    first commit

---
+++ app/src/main/java/rose/com/whatsappnoplatform/MainActivity.java
@@ -0,0 +1,33 @@
+package rose.com.whatsappnoplatform;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    public EditText addnumber ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        addnumber = findViewById(R.id.addNumber);
+
+
+    }
+
+    public void openNumber(View view){
+        String content = addnumber.getText().toString();
+        String number = "https://wa.me/"+content;
+        String fix = number;
+        Intent webIntent = new Intent(Intent.ACTION_VIEW ,Uri.parse(fix));
+        startActivity(webIntent);
+        addnumber.getText().clear();
+    }
+}
